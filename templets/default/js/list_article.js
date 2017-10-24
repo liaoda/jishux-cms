e622db852a567a1e307cc3f5d4a6eefe5306b51c
Trancy <dengqiangxi@icloud.com>
2017-10-24 23:56:06 +0800

    change time icon

--- templets/default/js/list_article.js
+++ templets/default/js/list_article.js
@@ -6,7 +6,7 @@ $(function () {
     $('.am-img-responsive').each(function (index, val) {
         if ($(this).attr('p-src') !== "/images/default_pic.png")  {
             var url = $(this).attr('p-src');
-            if (url.endsWith('_.jpg')) { url = url.substring(0, url.length - 5) + '/thumb'; }
+            if (url && url.endsWith('_.jpg')) { url = url.substring(0, url.length - 5) + '/thumb'; }
             $(this).attr('class', 'lazyload am-img-responsive').attr('data-src', url).attr('src', 'http://cdn.jishux.com/default_pic_thumb.png')
             $(this).parent().parent().next().attr('class', 'am-u-sm-9 am-list-main')
             $(this).error(function () { hideImage($(this)); })
