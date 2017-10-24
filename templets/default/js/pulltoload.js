cff05bda4c3ea1ed3f22bbf2532287d2311de002
TrancyDeng <dengqiangxi@icloud.com>
2017-10-24 14:39:45 +0800

    add inifity scroll

---
+++ templets/default/js/pulltoload.js
@@ -0,0 +1,45 @@
+
+var loadConfig = window.loadConfig
+function  loadMoreApply(){
+    if(loadConfig.loading === 0){
+        var typeid = loadConfig.typeid;
+        var page = loadConfig.page;
+        var pagesize = loadConfig.pagesize;
+        var url = loadConfig.url_api,data={ajax:'pullload',typeid:typeid,page:page,pagesize:pagesize};
+        var sTop = document.body.scrollTop || document.documentElement.scrollTop, dHeight = $(document).height(), cHeight = document.documentElement.clientHeight;
+
+        if (sTop + cHeight >= dHeight-20) {
+            $.AMUI.progress.start();
+            loadConfig.loading = 1;
+            function ajax(url, data) {
+                $.ajax({url: url,data: data,async: false,type: 'GET',dataType: 'json',success: function(data) {
+                    addContent(data);
+                }});
+            }
+            ajax(url,data);
+        }
+    }
+}
+function addContent (rs){
+    if(rs.statu== 1){
+        var data = rs.list;
+        var total = rs.total;
+        var arr=[];
+        var length = data.length;
+        for(var i=0;i<length;i++){
+            arr.push(data[i])
+        }
+        $('.am-list-news-bd>.am-list').append(arr.join(''));
+        loadConfig.load_num = rs.load_num;
+        if(total<loadConfig.page*loadConfig.pagesize || loadConfig.page > loadConfig.load_num){
+            window.removeEventListener('srcoll',loadMoreApply,false);
+        }
+        loadConfig.page++;
+        loadConfig.loading = 0;
+        $.AMUI.progress.done();
+    }
+}
+function pullLoad(){
+    window.addEventListener('scroll', loadMoreApply, false);
+}
+pullLoad();
