5fb8d8187bc932913533eece4843041144e1e9ee
TrancyDeng <dengqiangxi@icloud.com>
2017-10-24 12:05:29 +0800

    add inifity scroll

--- plus/list.php
+++ plus/list.php
@@ -46,7 +46,7 @@ if(isset($_GET['ajax'])){
             $row['isdefault'],$row['defaultname'],$row['ispart'],
             $row['namerule2'],$row['moresite'],$row['siteurl'],$row['sitepath']);
         if($row['litpic'] == '-' || $row['litpic'] == ''){
-            $row['litpic'] = $GLOBALS['cfg_cmspath'].'/images/defaultpic.gif';
+            $row['litpic'] = $GLOBALS['cfg_cmspath'].'/images/default_pic.png';
         }
         if(!preg_match("#^http:\/\/#i", $row['litpic']) &&$GLOBALS['cfg_multi_site'] == 'Y'){
             $row['litpic'] = $GLOBALS['cfg_mainsite'].$row['litpic'];
