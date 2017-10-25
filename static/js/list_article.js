81f9d88aa5517e6256d319c8152de7c945200a11
TrancyDeng <dengqiangxi@icloud.com>
2017-10-25 14:26:23 +0800

    compress js

--- static/js/list_article.js
+++ static/js/list_article.js
@@ -1,1 +1,15 @@
-$(function(){function a(b){b.parent().parent().css("display","none");b.parent().parent().next().attr("class","am-u-sm-12 am-list-main")}$(".am-img-responsive").each(function(c,d){if($(this).attr("p-src")!=="/images/default_pic.png"){var b=$(this).attr("p-src");if(b&&b.endsWith("_.jpg")){b=b.substring(0,b.length-5)+"/thumb"}$(this).attr("class","lazyload am-img-responsive").attr("data-src",b).attr("src","http://cdn.jishux.com/default_pic_thumb.png");$(this).parent().parent().next().attr("class","am-u-sm-9 am-list-main");$(this).error(function(){a($(this))})}})});
+$(function () {
+    function hideImage(that) {
+        that.parent().parent().css('display', 'none');
+        that.parent().parent().next().attr('class', 'am-u-sm-12 am-u-md-12 am-list-main')
+    }
+    $('.am-img-responsive').each(function (index, val) {
+        if ($(this).attr('p-src') !== "/images/default_pic.png")  {
+            var url = $(this).attr('p-src');
+            if (url && url.endsWith('_.jpg')) { url = url.substring(0, url.length - 5) + '/thumb'; }
+            $(this).attr('class', 'lazyload am-img-responsive').attr('data-src', url).attr('src', 'http://cdn.jishux.com/default_pic_thumb.png')
+            $(this).parent().parent().next().attr('class', 'am-u-sm-9 am-u-md-10 am-list-main')
+            $(this).error(function () { hideImage($(this)); })
+        }
+    })
+})
--- templets/default/js/list_article.js
+++ templets/default/js/list_article.js
@@ -1,14 +1,14 @@
 $(function () {
     function hideImage(that) {
-        that.parent().parent().css('display', 'none')
-        that.parent().parent().next().attr('class', 'am-u-sm-12 am-list-main')
+        that.parent().parent().css('display', 'none');
+        that.parent().parent().next().attr('class', 'am-u-sm-12 am-u-md-12 am-list-main')
     }
     $('.am-img-responsive').each(function (index, val) {
         if ($(this).attr('p-src') !== "/images/default_pic.png")  {
             var url = $(this).attr('p-src');
             if (url && url.endsWith('_.jpg')) { url = url.substring(0, url.length - 5) + '/thumb'; }
             $(this).attr('class', 'lazyload am-img-responsive').attr('data-src', url).attr('src', 'http://cdn.jishux.com/default_pic_thumb.png')
-            $(this).parent().parent().next().attr('class', 'am-u-sm-9 am-list-main')
+            $(this).parent().parent().next().attr('class', 'am-u-sm-9 am-u-md-10 am-list-main')
             $(this).error(function () { hideImage($(this)); })
         }
     })
