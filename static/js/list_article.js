de8e79ea687fb90013757689a04607ffec3b7833
TrancyDeng <dengqiangxi@icloud.com>
2017-10-25 14:33:02 +0800

    compress js

--- static/js/list_article.js
+++ static/js/list_article.js
@@ -1,15 +1,2 @@
-$(function () {
-    function hideImage(that) {
-        that.parent().parent().css('display', 'none');
-        that.parent().parent().next().attr('class', 'am-u-sm-12 am-u-md-12 am-list-main')
-    }
-    $('.am-img-responsive').each(function (index, val) {
-        if ($(this).attr('p-src') !== "/images/default_pic.png")  {
-            var url = $(this).attr('p-src');
-            if (url && url.endsWith('_.jpg')) { url = url.substring(0, url.length - 5) + '/thumb'; }
-            $(this).attr('class', 'lazyload am-img-responsive').attr('data-src', url).attr('src', 'http://cdn.jishux.com/default_pic_thumb.png')
-            $(this).parent().parent().next().attr('class', 'am-u-sm-9 am-u-md-10 am-list-main')
-            $(this).error(function () { hideImage($(this)); })
-        }
-    })
-})
+$(function(){$(".am-img-responsive").each(function(a,b){"/images/default_pic.png"!==$(this).attr("p-src")&&((a=$(this).attr("p-src"))&&a.endsWith("_.jpg")&&(a=a.substring(0,a.length-5)+"/thumb"),$(this).attr("class","lazyload am-img-responsive").attr("data-src",a).attr("src","http://cdn.jishux.com/default_pic_thumb.png"),$(this).parent().parent().next().attr("class","am-u-sm-9 am-u-md-10 am-list-main"),$(this).error(function(){var a=$(this);a.parent().parent().css("display","none");a.parent().parent().next().attr("class",
+    "am-u-sm-12 am-u-md-12 am-list-main")}))})});
