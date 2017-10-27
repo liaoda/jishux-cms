fb4cbc806afc2bd02a65cb789456dde57a5b901a
Trancy <dengqiangxi@icloud.com>
2017-10-27 14:08:24 +0800

    edit contract us

--- static/js/postcomment.js
+++ static/js/postcomment.js
@@ -36,25 +36,18 @@ function PostComment() {
 
     var validate = f.find('#validate');
     var verfity_code = validate.val()
-    if (verfity_code.length!==4) {
+    if (verfity_code.length !== 4) {
         alert("请填写验证码！");
         return;
     }
     else {
-        nvalidate =verfity_code
+        nvalidate = verfity_code
     }
 
     if (msg.length > 500) {
         alert("你的评论是不是太长了？请填写500字以内的评论。");
         return;
     }
-    // if (f.feedbacktype) {
-    //     for (var i = 0; i < f.feedbacktype.length; i++)
-    //         if (f.feedbacktype[i].checked) nfeedbacktype = f.feedbacktype[i].value;
-    // }
-    // location.href="#newcomment";
-
-    // if (f.username) nusername = f.username.value;
     $msg.text('')
     var data = {
         sendlang: 'zh-CN',
@@ -73,28 +66,13 @@ function PostComment() {
 
     $.ajax({
         url: '/plus/feedback_ajax.php', data: data, async: true, type: 'POST', success: function (data) {
-           console.log(data)
+            commentList.append(data)
+
         }
     });
-    // var myajax = new DedeAjax(taget_obj, false, true, '', '', waithtml);
-    // myajax.sendlang = '{dede:global.cfg_soft_lang/}';
-    // myajax.AddKeyN('dopost', 'send');
-    // myajax.AddKeyN('aid', '{dede:field.id/}');
-    // myajax.AddKeyN('fid', f.fid.value);
-    // myajax.AddKeyN('face', nface);
-    // myajax.AddKeyN('feedbacktype', nfeedbacktype);
-    // myajax.AddKeyN('validate', nvalidate);
-    // myajax.AddKeyN('notuser', nnotuser);
-    // myajax.AddKeyN('username', nusername);
-    // myajax.AddKeyN('pwd', npwd);
-    // myajax.AddKeyN('msg', msg);
-    // myajax.SendPost2('/plus/feedback_ajax.php');
-    //msg = '';
-    // CKEDITOR.instances.msg.setData('');
-    $msg.text('')
-    // document.getElementById("msg").value = "";
 
-    //taget_obj.removeAttribute('id');
+    $msg.text('')
+
     f.find('#feedbackfid').val('')
     if (validate.val()) {
         var $validateimg = $('#validateimg');
