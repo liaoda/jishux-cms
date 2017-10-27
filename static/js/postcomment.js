15424e96e4fc89e1ad271d369f269216db5f9d28
Trancy <dengqiangxi@icloud.com>
2017-10-27 16:11:27 +0800

    edit contract us

--- static/js/postcomment.js
+++ static/js/postcomment.js
@@ -1,6 +1,50 @@
 var aid = $('.blog-g-fixed').attr('aid');
 var commentList = $('#ulcommentlist')
 
+function postBadGood(ftype,fid)
+{
+    var comment_floor = $('#'+ftype+fid);
+    var saveid = Cookies.get('badgoodid');
+    if(saveid)
+    {
+        console.log(saveid)
+        var saveids = saveid.split(',');
+        var hasid = false;
+        saveid = '';
+        j = 1;
+        for(i=saveids.length-1;i>=0;i--)
+        {
+            if(saveids[i]===fid && hasid)
+            {}
+            else {
+                if(saveids[i]===fid && !hasid) hasid = true;
+                saveid += (saveid==='' ? saveids[i] : ','+saveids[i]);
+                j++;
+                if(j===10 && hasid) break;
+                if(j===9 && !hasid) break;
+            }
+        }
+        if(hasid) { comment_floor.popover({
+            content: '一次就好~'
+        }); return; }
+        else saveid += ','+fid;
+        Cookies.set('badgoodid',saveid,{ expires: 1 });
+    }
+    else
+    {
+        Cookies.set('badgoodid',fid,{ expires: 1 });
+    }
+
+    var like_count = comment_floor.find('li')
+
+    if(!action){
+        action='num'
+    }
+    $.get("/plus/feedback.php?action="+ftype+"&formurl=caicai"+"&aid="+fid+'fid='+fid,function(data,status){
+        like_count.text(data)
+    });
+
+}
 function LoadCommets(page) {
     var taget_obj = $('#commetcontent').text()
     var waithtml = "<div style='line-height:50px'><img src='/plus/images/loadinglit.gif' />评论加载中...</div>";
