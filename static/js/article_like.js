421d7c3a66caf1b26dcd3af660b9541ca56b44b3
Trancy <dengqiangxi@icloud.com>
2017-10-27 15:37:03 +0800

    edit contract us

--- static/js/article_like.js
+++ static/js/article_like.js
@@ -1,1 +1,98 @@
-$(function(){var c=$(".blog-g-fixed").attr("aid");function b(d){var e=$("#like-count");if(!d){d="num"}$.get("/plus/digg_ajax.php?action="+d+"&formurl=caicai"+"&id="+c,function(g,f){e.text(g)})}function a(){var f=Cookies.get("diggid");if(f){console.log(f);var d=f.split(",");var e=false;f="";j=1;for(i=d.length-1;i>=0;i--){if(d[i]===c&&e){}else{if(d[i]===c&&!e){e=true}f+=(f===""?d[i]:","+d[i]);j++;if(j===20&&e){break}if(j===19&&!e){break}}}if(e){$("#btn-like").popover({content:"一次就好~"});return}else{f+=","+c}Cookies.set("diggid",f,{expires:1});b("good")}else{Cookies.set("diggid",c,{expires:1});b("good")}}$("#btn-like").click(function(){a()});b()});
+$(function () {
+    var aid= $('.blog-g-fixed').attr('aid');
+    function getDigg(action) {
+        var like_count = $('#like-count')
+        if(!action){
+            action='num'
+        }
+        $.get("/plus/digg_ajax.php?action="+action+"&formurl=caicai"+"&id="+aid,function(data,status){
+            like_count.text(data)
+        });
+    }
+    function postGood()
+    {
+        var saveid = Cookies.get('diggid');
+        if(saveid)
+        {
+            console.log(saveid)
+            var saveids = saveid.split(',');
+            var hasid = false;
+            saveid = '';
+            j = 1;
+            for(i=saveids.length-1;i>=0;i--)
+            {
+                if(saveids[i]===aid && hasid)
+                {}
+                else {
+                    if(saveids[i]===aid && !hasid) hasid = true;
+                    saveid += (saveid==='' ? saveids[i] : ','+saveids[i]);
+                    j++;
+                    if(j===20 && hasid) break;
+                    if(j===19 && !hasid) break;
+                }
+            }
+            if(hasid) { $('#btn-like').popover({
+                content: '一次就好~'
+            }); return; }
+            else saveid += ','+aid;
+            Cookies.set('diggid',saveid,{ expires: 1 });
+            getDigg('good')
+        }
+        else
+        {
+            Cookies.set('diggid',aid,{ expires: 1 });
+            getDigg('good')
+        }
+    }
+
+
+    function postBadGood(ftype,fid)
+    {
+        var comment_floor = $('#'+ftype+fid);
+        var saveid = Cookies.get('badgoodid');
+        if(saveid)
+        {
+            console.log(saveid)
+            var saveids = saveid.split(',');
+            var hasid = false;
+            saveid = '';
+            j = 1;
+            for(i=saveids.length-1;i>=0;i--)
+            {
+                if(saveids[i]===fid && hasid)
+                {}
+                else {
+                    if(saveids[i]===fid && !hasid) hasid = true;
+                    saveid += (saveid==='' ? saveids[i] : ','+saveids[i]);
+                    j++;
+                    if(j===10 && hasid) break;
+                    if(j===9 && !hasid) break;
+                }
+            }
+            if(hasid) { comment_floor.popover({
+                content: '一次就好~'
+            }); return; }
+            else saveid += ','+fid;
+            Cookies.set('badgoodid',saveid,{ expires: 1 });
+        }
+        else
+        {
+            Cookies.set('badgoodid',fid,{ expires: 1 });
+        }
+
+        var like_count = comment_floor.find('li')
+
+        if(!action){
+            action='num'
+        }
+        $.get("/plus/feedback.php?action="+ftype+"&formurl=caicai"+"&aid="+fid+'fid='+fid,function(data,status){
+            like_count.text(data)
+        });
+
+    }
+
+    $('#btn-like').click(function () {
+        postGood()
+    })
+    getDigg()
+})
