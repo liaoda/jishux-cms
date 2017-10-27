f5b1d808873b05ff963873ba306e1584dbd99dd5
Trancy <dengqiangxi@icloud.com>
2017-10-27 13:48:54 +0800

    edit contract us

--- static/js/postcomment.js
+++ static/js/postcomment.js
@@ -1,21 +1,23 @@
-var aid= $('.blog-g-fixed').attr('aid');
+var aid = $('.blog-g-fixed').attr('aid');
 var commentList = $('#ulcommentlist')
-function LoadCommets(page)
-{
+
+function LoadCommets(page) {
     var taget_obj = $('#commetcontent').text()
     var waithtml = "<div style='line-height:50px'><img src='/plus/images/loadinglit.gif' />评论加载中...</div>";
     var data = {
-        dopost:'getlist',
-        aid:aid,
-        page:page
+        dopost: 'getlist',
+        aid: aid,
+        page: page
     }
-    $.ajax({url: '/plus/feedback_ajax.php',data: data,async: true,type: 'GET',success: function(data) {
-        commentList.append(data)
-    }});
+    $.ajax({
+        url: '/plus/feedback_ajax.php', data: data, async: true, type: 'GET', success: function (data) {
+            commentList.append(data)
+        }
+    });
 
 }
-function PostComment()
-{
+
+function PostComment() {
 
     var f = $('.am-form')
     var $msg = $('#msg');
@@ -25,59 +27,55 @@ function PostComment()
     var nfeedbacktype = 'feedback';
     var nvalidate = '';
     var nnotuser = '1';
-    var nusername = '';
-    var npwd = '';
-    var taget_obj = $('#commetcontentNew');
-    var waithtml = "<div style='line-height:30px'><img src='/plus/images/loadinglit.gif' />正在发送中...</div>";
-    if(msg.length < 10)
-    {
+    var nusername = '------';
+    if (msg.length < 10) {
         alert("请至少输入10个字！");
         return;
     }
 
-    if(f.validate)
-    {
-        var validate = f.find('#validate');
 
-        if(validate.val()) {
-            alert("请填写验证码！");
-            return;
-        }
-        else {
-            nvalidate = f.validate.value;
-        }
+    var validate = f.find('#validate');
+    var verfity_code = validate.val()
+    if (verfity_code) {
+        alert("请填写验证码！");
+        return;
     }
-    if(msg.length > 500)
-    {
+    else {
+        nvalidate =verfity_code
+    }
+
+    if (msg.length > 500) {
         alert("你的评论是不是太长了？请填写500字以内的评论。");
         return;
     }
-    if(f.feedbacktype) {
-        for(var i=0; i < f.feedbacktype.length; i++)
-            if(f.feedbacktype[i].checked) nfeedbacktype = f.feedbacktype[i].value;
-    }
+    // if (f.feedbacktype) {
+    //     for (var i = 0; i < f.feedbacktype.length; i++)
+    //         if (f.feedbacktype[i].checked) nfeedbacktype = f.feedbacktype[i].value;
+    // }
     // location.href="#newcomment";
 
-    if(f.username) nusername = f.username.value;
+    // if (f.username) nusername = f.username.value;
     $msg.text('')
     var data = {
-        sendlang:'zh-CN',
-        dopost:'send',
-        aid:aid,
-        fid:f.find('#feedbackfid').val(),
-        face:nface,
-        feedbacktype:nfeedbacktype,
-        validate:nvalidate,
-        notuser:nnotuser,
-        username:nusername,
-        pwd:'-----pwd',
-        msg:msg,
+        sendlang: 'zh-CN',
+        dopost: 'send',
+        aid: aid,
+        fid: f.find('#feedbackfid').val(),
+        face: nface,
+        feedbacktype: nfeedbacktype,
+        validate: nvalidate,
+        notuser: nnotuser,
+        username: nusername,
+        pwd: '-----pwd',
+        msg: msg,
 
     }
 
-    $.ajax({url: '/plus/feedback_ajax.php',data: data,async: true,type: 'POST',success: function(data) {
-        commentList.append(data)
-    }});
+    $.ajax({
+        url: '/plus/feedback_ajax.php', data: data, async: true, type: 'POST', success: function (data) {
+            commentList.append(data)
+        }
+    });
     // var myajax = new DedeAjax(taget_obj, false, true, '', '', waithtml);
     // myajax.sendlang = '{dede:global.cfg_soft_lang/}';
     // myajax.AddKeyN('dopost', 'send');
@@ -98,15 +96,15 @@ function PostComment()
 
     //taget_obj.removeAttribute('id');
     f.find('#feedbackfid').val('')
-    if(validate.val())
-    {
+    if (validate.val()) {
         var $validateimg = $('#validateimg');
-        if($validateimg) $validateimg.attr('src',"/plus/include/vdimgck.php?"+f.validate.value) ;
+        if ($validateimg) $validateimg.attr('src', "/plus/include/vdimgck.php?" + f.validate.value);
         validate.val('')
     }
 
 
 }
+
 $('#btnComment').click(function () {
     PostComment()
 })
